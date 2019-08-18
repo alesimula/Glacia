da472a9ec14bac2e56a699d63c554b9fa8e60cc9
Alex Sim <ale.simula97@gmail.com>
2019-08-18 16:00:47 +0200

    Improve ReflectEnumConstructor

--- src/main/java/com/greenapple/glacia/delegate/ReflectEnumConstructor.kt
+++ src/main/java/com/greenapple/glacia/delegate/ReflectEnumConstructor.kt
@@ -5,21 +5,23 @@ import java.lang.reflect.Constructor
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 
-private val constructorAccessorField by lazy {Constructor::class.java.getDeclaredField("constructorAccessor").apply {isAccessible = true}}
+private val Constructor<*>.constructorAccessorKt : ConstructorAccessor? by ReflectField("constructorAccessor")
 private val constructorAccessorRetriever by lazy {Constructor::class.java.getDeclaredMethod("acquireConstructorAccessor").apply {isAccessible = true}}
 
-open class ReflectEnumConstructor<This: Enum<*>> (thisRef: KClass<This>) {
-    private val oreFeatureConfigConstructor by lazy {thisRef.java.declaredConstructors[0].run {
-        isAccessible = true
-        (constructorAccessorField.get(this) ?: constructorAccessorRetriever.invoke(this)) as ConstructorAccessor
+open class ReflectEnumConstructor<This: Enum<*>> (thisRef: KClass<This>, vararg args: KClass<*>) {
+    private val constructorAccessor by lazy { thisRef.java.run {
+        (if (args.isEmpty()) runCatching {getDeclaredConstructor(String::class.java, Int::class.java)}.getOrElse {declaredConstructors[0]} else getDeclaredConstructor(String::class.java, Int::class.java, *args.map {it.java}.toTypedArray())).run {
+            isAccessible = true
+            (constructorAccessorKt ?: constructorAccessorRetriever(this)) as ConstructorAccessor
+        }
     }}
     inner class EnumConstructor : (String, Array<out Any?>) -> This {
         @Suppress("UNCHECKED_CAST")
-        override fun invoke(enumName: String, vararg args: Any?): This = oreFeatureConfigConstructor.newInstance(arrayOf(enumName, -1, *args)) as This
+        override fun invoke(enumName: String, vararg args: Any?): This = constructorAccessor.newInstance(arrayOf(enumName, -1, *args)) as This
     }
+    private val constructor = EnumConstructor()
 
-    operator fun getValue(thisRef:This?,property:KProperty<*>) = EnumConstructor()
-    operator fun getValue(thisRef:Any?,property:KProperty<*>) = EnumConstructor()
+    operator fun getValue(thisRef:Any?,property:KProperty<*>) = constructor
 }
 
-inline fun <reified This: Enum<*>>ReflectEnumConstructor() = ReflectEnumConstructor(This::class)
+inline fun <reified This: Enum<*>>ReflectEnumConstructor(vararg args: KClass<*>) = ReflectEnumConstructor(This::class, *args)
