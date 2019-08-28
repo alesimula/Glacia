bf1c1f2b785d6b375ff8684a459a36ae6c7c33ea
Alex Sim <ale.simula97@gmail.com>
2019-08-28 16:20:27 +0200

    improve GlaciaEntityData

--- src/main/java/com/greenapple/glacia/entity/GlaciaEntityData.kt
+++ src/main/java/com/greenapple/glacia/entity/GlaciaEntityData.kt
@@ -9,24 +9,22 @@ import net.minecraft.network.datasync.DataParameter
 import net.minecraft.network.datasync.DataSerializers
 import net.minecraft.network.datasync.EntityDataManager
 import net.minecraft.network.datasync.IDataSerializer
+import java.lang.ref.WeakReference
 import java.util.*
 import kotlin.random.Random
 import kotlin.reflect.KProperty
 
 // <editor-fold defaultstate="collapsed" desc="Delegation methods">
-private val dataManagerCache : Cache<UUID, EntityDataManager> = CacheBuilder.newBuilder().weakKeys().build()
+//val dataManagerCache : Cache<UUID, EntityDataManager> = CacheBuilder.newBuilder().weakKeys().weakValues().build()
+val dataManagerCache : MutableMap<UUID, WeakReference<EntityDataManager>> = Collections.synchronizedMap(WeakHashMap<UUID, WeakReference<EntityDataManager>>())
 private class EntityDataDelegate<This: Entity, Return> (serializer: IDataSerializer<Return>, private val defaultProvider: This.()->Return) {
     val Class<This>.dataParameter : DataParameter<Return> by LazyWithReceiver(false) {EntityDataManager.createKey(this, serializer)}
-    val This.cachedDataManager; get() = dataManagerCache.getIfPresent(uniqueID)
+    val This.cachedDataManager : EntityDataManager; get() = dataManagerCache.putIfAbsent(uniqueID, WeakReference(dataManager))?.get() ?: dataManager
     val This.dataParameter; get() = this.javaClass.dataParameter
-    fun This.registerData(value: Return) : Return? {
-        return synchronized(uniqueID) {
-            cachedDataManager ?: dataManager.apply {dataManagerCache.put(uniqueID, this)}
-        }.runCatching {dataManager.register(dataParameter, value); value}.getOrNull()
-    }
+    fun This.registerData(value: Return) = cachedDataManager.runCatching {dataManager.register(dataParameter, value); value}.getOrNull()
 
-    operator fun getValue(thisRef:This, property: KProperty<*>) = defaultProvider(thisRef).let {default -> thisRef.registerData(default); runCatching{thisRef.cachedDataManager?.get(thisRef.dataParameter) ?: default}.getOrElse {default}}
-    operator fun setValue(thisRef:This, property: KProperty<*>, value: Return) {thisRef.registerData(value) ?: runCatching {thisRef.cachedDataManager?.set(thisRef.dataParameter, value)}}
+    operator fun getValue(thisRef:This, property: KProperty<*>) = thisRef.run {defaultProvider(this).let {default -> registerData(default) ?: runCatching{cachedDataManager.get(dataParameter) ?: default}.getOrElse {default}}}
+    operator fun setValue(thisRef:This, property: KProperty<*>, value: Return) = thisRef.apply {registerData(value) ?: runCatching {cachedDataManager.set(dataParameter, value)}}
 }
 private inline fun <reified This: Entity, Return> entityData(serializer: IDataSerializer<Return>, noinline defaultProvider: This.()->Return) = EntityDataDelegate(serializer, defaultProvider)
 // </editor-fold>
