eae3dea8b8a241e34f34e7651cff567bb6f47b30
Alex Sim <ale.simula97@gmail.com>
2019-09-01 10:27:57 +0200

    Fix PlayerEvents

--- src/main/java/com/greenapple/glacia/event/PlayerEvents.kt
+++ src/main/java/com/greenapple/glacia/event/PlayerEvents.kt
@@ -3,7 +3,6 @@ package com.greenapple.glacia.event
 import com.greenapple.glacia.block.BlockFireBase
 import net.minecraft.util.ActionResultType
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
-import net.minecraftforge.eventbus.api.Event
 import net.minecraftforge.eventbus.api.SubscribeEvent
 
 class PlayerEvents {
@@ -11,7 +10,7 @@ class PlayerEvents {
      * Code similar to World.extinguishFire
      */
     @SubscribeEvent
-    fun onRenderPlayerThirdPerson(event: PlayerInteractEvent.LeftClickBlock) = event.run {
+    fun onRenderPlayerThirdPerson(event: PlayerInteractEvent.LeftClickBlock) = event.apply {
         val blockPos = face?.run {pos.offset(this)} ?: pos
         if (world.getBlockState(blockPos).block is BlockFireBase) {
             world.playEvent(player, 1009, blockPos, 0)
